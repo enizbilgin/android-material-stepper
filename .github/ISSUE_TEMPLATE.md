5e5649dd08adb62d00ee7d28b72833d5a5bc7b0c
enizbilgin <enizbilgin@users.noreply.github.com>
2017-06-10 20:22:10 +0300

    Revert "Release 3.3.0" (#4)

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,9 +0,0 @@
-Make sure these boxes are checked before submitting your issue - thank you!
-
-- [ ] Check in the issue tracker whether
-      this issue wasn't already reported.
-- [ ] If it's a bug report check that clear **steps to reproduce**,
-**stacktrace**, **expected behaviour** and other needed details are provided.
-- [ ] Feature requests contain a **description** of the feature
-you're requesting as well as a brief **scenario** explaining
-under what circumstances this might be useful.
--- material-stepper/src/test/java/com/stepstone/stepper/test/assertion/StepperLayoutAssert.kt
+++ material-stepper/src/test/java/com/stepstone/stepper/test/assertion/StepperLayoutAssert.kt
@@ -1,8 +1,11 @@
 package com.stepstone.stepper.test.assertion
 
 import android.support.annotation.IdRes
+import android.view.View
+
 import com.stepstone.stepper.R
 import com.stepstone.stepper.StepperLayout
+
 import org.assertj.android.api.Assertions
 import org.assertj.android.api.view.ViewAssert
 import org.assertj.android.api.widget.AbstractLinearLayoutAssert
@@ -47,16 +50,6 @@ class StepperLayoutAssert constructor(actual: StepperLayout) : AbstractLinearLay
 
     fun hasTabsHidden(): StepperLayoutAssert {
         hasNotNullChildView(R.id.ms_stepTabsContainer).isGone
-        return this
-    }
-
-    fun hasBottomNavigationShown(): StepperLayoutAssert {
-        hasNotNullChildView(R.id.ms_bottomNavigation).isVisible
-        return this
-    }
-
-    fun hasBottomNavigationHidden(): StepperLayoutAssert {
-        hasNotNullChildView(R.id.ms_bottomNavigation).isGone
         return this
     }
 
